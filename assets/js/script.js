919cc8b210ec3b6b011b20aa9c9d61487daf9459
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-13 16:05:52 -0500

    final commit of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var weatherContainerEl = document.querySelector("#current-weather");
 var citySearchEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city-search");
-var cityButtonsEl = document.querySelector("#city-buttons")
+var cityButtonsEl = document.querySelector("#city-buttons");
+var citySearchTermEl = document.querySelector("#city-search-term")
 
 
 var cityButtonsEl = document.querySelector("#city-buttons");
@@ -29,12 +30,11 @@ function getCityWeather(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q" + city + "&appid=8a6335987062d51ad7d8c2a8d96bc7cc";;
   
     // make a request to the url
-    fetch(apiUrl)
-    .then(function(response) {
+    fetch(apiUrl).then(function(response) {
       // request was successful
       if (response.ok) {
         response.json().then(function(data) {
-          displayWeather(data, weather);
+          displayWeather(data.items, city);
         });
       } else {
         alert('Error: City not found');
@@ -51,6 +51,24 @@ function getCityWeather(city) {
   
     fetch(apiUrl);
   };
+
+  function displayWeather(weather, searchTerm) {
+    weatherSearchTerm.textContent = searchTerm;
+
+    var tempEl = document.createElement("a");
+    tempEl.classList = "list-item flex-row";
+    
+    var windEl = document.createElement("a");
+    windEl.classlist = "list-item flex-row";
+
+    var humidityEl = document.createElement("a");
+    humidityEl.classlist = "list-item flex-row";
+
+    var uvIndexEl = document.createElement("a");
+    uvIndexEl.classlist = "list-item flex-row";
+
+
+  }
 
   
 
