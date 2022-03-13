e7ac05afc559040561670f7c7eff81880bb14559
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-13 14:39:55 -0500

    added city buttons with unique data tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-function getDenverWeather() {
+weatherContainerEl = document.querySelector("#current-weather")
+
+var cityButtonsEl = document.querySelector("#city-buttons");
+
+function getCityWeather() {
     // format the OPEN WEATHER api url
     var apiUrl = "OPEN WEATHER API";
   
@@ -8,7 +12,7 @@ function getDenverWeather() {
       // request was successful
       if (response.ok) {
         response.json().then(function(data) {
-          displayWeather(data, user);
+          displayWeather(data, weather);
         });
       } else {
         alert('Error: City not found');
@@ -21,7 +25,7 @@ function getDenverWeather() {
   };
 
   function getCurrentWeather() {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={API key}";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid={8a6335987062d51ad7d8c2a8d96bc7cc}";
   
     fetch(apiUrl);
   };
